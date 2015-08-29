3bb7c8c4a5b24d3abcab7242e90fbb76ea23d03d
luduvigo <paoloantoniorossi@gmail.com>
2015-08-29 18:09:56 +0200

    Some progress

--- server.js
+++ server.js
@@ -2,13 +2,35 @@ var express = require("express")
 var bodyParser = require("body-parser")
 var Pitch = require("./models/pitch")
 
-var app = express()
+var app = express(),
+    http = require('http'),
+    cons = require('consolidate'),
+    stylus = require('stylus');
+
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//set path to the views (template) directory
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'jade');
+
+app.use(stylus.middleware({
+    debug: true,
+    src: __dirname + '/views',
+    dest: __dirname + '/public'
+}));  
+app.use(express.static(__dirname + '/public'));
+
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + "/views");
+
 app.use("/api/pitches", require("./controllers/api/pitches"))
 app.use(require("./controllers/static"))
-
 app.listen(3000, function() {
 	console.log("Server listening on", 3000)
 })
