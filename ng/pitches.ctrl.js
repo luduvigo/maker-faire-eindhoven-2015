2d50979bfe453e038c2bece167e69ce56724a100
luduvigo <paoloantoniorossi@gmail.com>
2015-08-29 21:32:12 +0200

    some stuff in the homepage

--- ng/pitches.ctrl.js
+++ ng/pitches.ctrl.js
@@ -7,12 +7,10 @@ angular.module('app')
                     votes : 0
                 }).
                 success(function (pitch) {
-                    alert('success')
                     $scope.pitches.unshift(pitch)
                     $scope.postBody = null         
                 })
             }
-            alert('there')
         //}
         PitchesSvc.fetch().success(function (pitches) {
             $scope.pitches = pitches
