8ec80415362e01ea80ef81c5ddc81c5aeeeef875
luduvigo <paoloantoniorossi@gmail.com>
2015-08-30 12:56:51 +0200

    Change db. Remove mock data.

--- controllers/api/pitches.js
+++ controllers/api/pitches.js
@@ -22,12 +22,7 @@ router.post("/", function(req, res, next){
 	//})
     
 	var pitch = new Pitch({
-		title: 'Pillo Development Kit',
-	votes: 168,
-        description: 'For all crazy developers with Pillo controllers, here you’ll find the available software to support creating Pillo games in Unity 3D.',
-        tag: 'tag',
-        pictureurl: 'http://pillogames.com/wp-content/uploads/2013/01/Pdk_pillo-pressure.png',
-        labname: 'Pillo Team'
+		
     })
 	pitch.save(function (err, pitch) {
 		if(err) { return next(err) }
--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost/eindhoven2015_1", function(req, res){
+mongoose.connect("mongodb://localhost/eindhoven2015_2", function(req, res){
 	console.log("Mongo DB connected")
 })
 module.exports = mongoose
