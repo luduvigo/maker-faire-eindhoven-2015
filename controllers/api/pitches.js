d0d4cfbabe80c2ff93e5cc297a5e6d382b491204
luduvigo <paoloantoniorossi@gmail.com>
2015-08-30 11:45:36 +0200

    Change order

--- controllers/api/pitches.js
+++ controllers/api/pitches.js
@@ -22,12 +22,12 @@ router.post("/", function(req, res, next){
 	//})
     
 	var pitch = new Pitch({
-		title: 'CityTV.nl',
-		votes: 150,
-        description: 'Live internet broadcasts from 25 Cities, ...and counting.',
+		title: 'Power Field Media',
+		votes: 235,
+        description: 'We love to capture wisdom and to build on interactions.',
         tag: 'tag',
         pictureurl: 'https://yt3.ggpht.com/-Ar8UAZslucw/AAAAAAAAAAI/AAAAAAAAAAA/mY9aC8XHlrU/s100-c-k-no/photo.jpg',
-        labname: 'CityTV team'
+        labname: 'Brian Kragtwijk'
     })
 	pitch.save(function (err, pitch) {
 		if(err) { return next(err) }
